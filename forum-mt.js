179a8643308738efc12a489fcc2719c3877a37bf
Matthew Bunday <zenceph@gmail.com>
2013-11-14 18:59:27 -0500

    Some user/author creation.

--- forum-mt.js
+++ forum-mt.js
@@ -1,6 +1,43 @@
+Authors = new Meteor.Collection("authors");
+Conversations = new Meteor.Collection("conversations");
+Comments = new Meteor.Collection("comments");
+Mentions = new Meteor.Collection("mentions");
+
+if (Meteor.isServer) {
+  createAuthor = function(user) {
+    author = {};
+    author['user_id'] = user._id;
+    author['name'] = user.username;
+    author['join_date'] = user.createdAt;
+    author['conversations'] = 0;
+    author['comments'] = 0;
+    author['subscriptions'] = {};
+    Authors.insert(author);
+  };
+
+  Accounts.onCreateUser(function(options, user) {
+    console.log(options);
+    console.log(user);
+    createAuthor(user);
+    return user;
+  });
+
+  Meteor.startup(function () {
+    Meteor.publish("authors", function() {
+      return Authors.find({});
+    });
+
+    // code to run on server at startup
+  });
+}
+
+
 if (Meteor.isClient) {
+  Meteor.subscribe("authors");
+
   Template.hello.greeting = function () {
-    return "Welcome to forum-mt.";
+    if (Authors.findOne({}) !== undefined)
+      return Authors.findOne({}).name;
   };
 
   Template.hello.events({
@@ -10,10 +47,8 @@ if (Meteor.isClient) {
         console.log("You pressed the button");
     }
   });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
   });
 }
